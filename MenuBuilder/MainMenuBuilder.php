d5395f55370f7967e4cc2202ace8a92830bff753
Raynald <raynald@easi-services.fr>
2018-01-12 19:49:05 +0100

    Add target and url to MainMenuBuilder

--- MenuBuilder/MainMenuBuilder.php
+++ MenuBuilder/MainMenuBuilder.php
@@ -28,9 +28,9 @@ class MainMenuBuilder implements MenuBuilderInterface
     protected $mainMenu;
     protected $request;
     protected $authorizationChecker;
-
-    public function __construct($menuEntity, FactoryInterface $factoryInterface, ManagerRegistry $managerRegistry, RequestStack $requestStack, ChainedRouterInterface $routerInterface, 
-                                SiteSelectorInterface $siteSelectorInterface, AuthorizationCheckerInterface $authorizationChecker)
+    
+    public function __construct($menuEntity, FactoryInterface $factoryInterface, ManagerRegistry $managerRegistry, RequestStack $requestStack, ChainedRouterInterface $routerInterface,
+        SiteSelectorInterface $siteSelectorInterface, AuthorizationCheckerInterface $authorizationChecker)
     {
         $this->menuEntity               = $menuEntity;
         $this->factoryInterface         = $factoryInterface;
@@ -48,108 +48,115 @@ class MainMenuBuilder implements MenuBuilderInterface
             return;
         }
         $this->renderMenu($menuType);
-
-
+        
+        
         return $this->mainMenu;
     }
-
+    
     public function getCurrentMenuName()
     {
         return $this->currentMenuName;
     }
-
+    
     
     protected function renderMenu(MenuType $menuType)
     {
         $site    = $this->siteSelectorInterface->retrieve();
-
+        
         //$menus   = $this->managerRegistry->getRepository($this->menuEntity)->findBy(array("site" => $site->getId(), "parent" => null), array("root" => "ASC", "lft" => "ASC"));
         $menus   = $this->managerRegistry->getRepository($this->menuEntity)->getMenus($site, $menuType);
-
+        
         if (count($menus) == 0) {
             $this->mainMenu = $this->factoryInterface->createItem('root');
-
+            
             return;
         }
-
+        
         $this->createMenuStructure($menus);
     }
-
+    
     protected function createMenuStructure($menus, ItemInterface $root = null)
     {
         foreach ($menus as $menu) {
             $this->createMenu($menu, $root);
         }
     }
-
+    
     protected function createMenu(Menu $menu, ItemInterface $root = null)
     {
-        if((null !== $menu->getParent() && $menu->getParent()->getUserRestricted() || $menu->getUserRestricted()) && 
+        if((null !== $menu->getParent() && $menu->getParent()->getUserRestricted() || $menu->getUserRestricted()) &&
             !$this->authorizationChecker->isGranted('ROLE_USER')) {
-            return false;
-        }
-
-        if($menu->getHideWhenUserConnected() && $this->authorizationChecker->isGranted('ROLE_USER')) {
-            return false;
-        }
-        
-        $currentItem = $this->createMenuItem($menu);
-
-        $level = $menu->getLvl();
-
-        if ($level == 0) {
-            $this->mainMenu = $currentItem;
-        } else {
-            $currentMenu = $root->addChild($currentItem);
-            if (null !== $menu->getIcon()) {
-                $currentMenu->setExtra('icon', $menu->getIcon());
+                return false;
             }
-            if ($level == 1 && $menu->getChildren()->count() > 0) {
-                $currentMenu->setExtra('dropdown', true);
+            
+            if($menu->getHideWhenUserConnected() && $this->authorizationChecker->isGranted('ROLE_USER')) {
+                return false;
             }
-        }
-
-        if (count($menu->getChildren()) > 0 && ($menu->getActive() or $level == 0)) {
+            
+            $currentItem = $this->createMenuItem($menu);
+            
+            $level = $menu->getLvl();
+            
             if ($level == 0) {
-                $this->putRootAttributes($currentItem);
+                $this->mainMenu = $currentItem;
             } else {
-                $this->putChildAttributes($currentItem);
+                $currentMenu = $root->addChild($currentItem);
+                if (null !== $menu->getIcon()) {
+                    $currentMenu->setExtra('icon', $menu->getIcon());
+                }
+                if ($level == 1 && $menu->getChildren()->count() > 0) {
+                    $currentMenu->setExtra('dropdown', true);
+                }
             }
-
-            $this->createMenuStructure($menu->getChildren(), $currentItem);
-        }
+            
+            if (count($menu->getChildren()) > 0 && ($menu->getActive() or $level == 0)) {
+                if ($level == 0) {
+                    $this->putRootAttributes($currentItem);
+                } else {
+                    $this->putChildAttributes($currentItem);
+                }
+                
+                $this->createMenuStructure($menu->getChildren(), $currentItem);
+            }
     }
-
+    
     protected function createMenuItem(Menu $menu)
     {
         $current = $this->factoryInterface->createItem($menu->getName(), array('label' => $menu->getName()));
-
+        
         if ($menu->getClickable() && null !== $menu->getPage()) {
             $this->createLink($current, $menu);
         }
-
+        if ($menu->getClickable() && null === $menu->getPage() && null !== $menu->getUrl()) {
+            $current->setUri($menu->getUrl());
+        }
+        
+        if($menu->getTarget() !== null && $menu->getTarget() !== '_self') {
+            $current->setLinkAttribute('target', $menu->getTarget());
+        }
+        
         $matcher = new Matcher();
         $matcher->addVoter(new UriVoter($_SERVER['REQUEST_URI']));
         if ($matcher->isCurrent($current)) {
             $current->setCurrent(true);
             $this->currentMenuName = $menu->getName();
         }
-
+        
         return $current;
     }
-
+    
     protected function createLink(ItemInterface $itemInterface, Menu $menu)
     {
         $uri = $this->routerInterface->generate($menu->getPage());
-
+        
         $itemInterface->setUri($uri);
     }
-
+    
     protected function putRootAttributes(ItemInterface $itemInterface)
     {
         $itemInterface->setChildrenAttribute("class", "nav navbar-nav");
     }
-
+    
     protected function putChildAttributes(ItemInterface $itemInterface)
     {
         $itemInterface->setAttribute("class", "");
